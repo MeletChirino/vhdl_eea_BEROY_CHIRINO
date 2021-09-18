18aaef42ce3b2d5932cea2f9c4abd8219f4a701a
Melet <melet.chirino@gmail.com>
2021-09-18 09:49:58 +0200

    [UPDATED] Testbench for shift register

--- BE_Barre_Franche/gestion_verin/gestion_verin_tb.vhdl
+++ BE_Barre_Franche/gestion_verin/gestion_verin_tb.vhdl
@@ -11,21 +11,34 @@ entity gestion_verin_tb is
 end entity;
 
 architecture test of gestion_verin_tb is
-	signal clk	: std_logic := '0';
-	signal clk_adc	: std_logic := '0';
+	signal clk_50M	: std_logic := '1';
+	signal clk_adc	: std_logic := '1';
+	signal data	: std_logic_vector(11 downto 0) := x"abc";
+	signal data_in	: std_logic := data(0);
+	signal final_data	: std_logic_vector(11 downto 0);
+	signal i	: integer := 0;
 begin
+	clk_50		: clk_50MHz port map(
+		output		=> clk_50M
+				    );
 	clk_adc_p	: clk_1MHz  port map(
 		--internal signal => external signal
-		clk_in		=> clk,
+		clk_in		=> clk_50M,
 		clk_out		=> clk_adc
 	);
-	process begin
-		clk <= '1';
-		for i in 0 to 400 loop
-			wait for 10 ns;
-			clk <= '0';
-			wait for 10 ns;
-			clk <= '1';
-		end loop;
-	end process;
+	shift_reg	: shift_register port map(
+		data_in		=> data_in,
+		clk_in		=> clk_adc
+		--angle_barre	=> final_data		
+	);
+	data_load	: process(clk_adc) is
+	begin
+		if (clk_adc'event and clk_adc ='1') then
+			data_in <= data(i);
+			i <= i + 1;
+		end if;
+		if (i = 12) then
+			i <= 0;
+		end if;
+	end process data_load;
 end test;
