c26cf8b694758317907f6d793d0fe33ba064c26d
EtienneLemaire <etienne.lemaire38@gmail.com>
2021-09-21 10:58:29 +0200

    [ADD] gestion verin as component

---
+++ BE_Barre_Franche/gestion_verin/gestion_verin.vhdl
@@ -0,0 +1,19 @@
+-- Filr 
+library	ieee;
+use	ieee.std_logic_1164.all;
+use 	ieee.std_logic_unsigned.all;
+use	ieee.numeric_std.all;
+
+--my components
+library work;
+use	work.tools.all;
+
+entity gestion_verin is
+end entity;
+
+architecture rtl of gestion_verin is
+
+begin
+	
+
+end rtl;
