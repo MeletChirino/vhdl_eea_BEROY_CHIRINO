cb0b21392432396a1e8d48ef60a649f798a291cc
Melet David <melet.chirino@gmail.com>
2021-11-09 09:12:11 +0100

    [ADD] spoc v3

---
+++ BE_Barre_Franche/gestion_verin/sopc_v3/sopc_v3_inst.v
@@ -0,0 +1,12 @@
+	sopc_v3 u0 (
+		.butee_d_external_connection_export     (<connected-to-butee_d_external_connection_export>),     //     butee_d_external_connection.export
+		.butee_g_external_connection_export     (<connected-to-butee_g_external_connection_export>),     //     butee_g_external_connection.export
+		.clk_clk                                (<connected-to-clk_clk>),                                //                             clk.clk
+		.duty_external_connection_export        (<connected-to-duty_external_connection_export>),        //        duty_external_connection.export
+		.frequency_external_connection_export   (<connected-to-frequency_external_connection_export>),   //   frequency_external_connection.export
+		.angle_barre_external_connection_export (<connected-to-angle_barre_external_connection_export>), // angle_barre_external_connection.export
+		.read_data_external_connection_export   (<connected-to-read_data_external_connection_export>),   //   read_data_external_connection.export
+		.write_data_external_connection_export  (<connected-to-write_data_external_connection_export>),  //  write_data_external_connection.export
+		.sens_external_connection_export        (<connected-to-sens_external_connection_export>)         //        sens_external_connection.export
+	);
+
