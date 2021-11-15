c8566393323bc2c4fa382434a0a515f855e0378e
Melet David <melet.chirino@gmail.com>
2021-11-15 11:17:42 +0100

    [ADD] Compt front to shift_register

--- BE_Barre_Franche/gestion_verin/shift_register.vhdl
+++ BE_Barre_Franche/gestion_verin/shift_register.vhdl
@@ -7,9 +7,9 @@ use	ieee.numeric_std.all;
 entity shift_register is
 	port(
 		enable		: in std_logic;
-		number		: in std_logic_vector(3 downto 0);
 		data_in		: in std_logic;
 		angle_barre	: out std_logic_vector(0 to 11) := x"000";
+		fin_c		: out std_logic := '0';
 		clk_in		: in std_logic := '0'
 	);
 end entity;
@@ -17,21 +17,36 @@ end entity;
 architecture rtl of shift_register is
 	signal buff_data	: std_logic_vector(0 to 11);
 	signal angle_barre_s	: std_logic_vector(0 to 11);
-	signal number_i		: integer range 0 to 13;
+	signal number		: integer range 0 to 13;
 begin
 	--rec_dec process
-	number_i <= to_integer(unsigned(number));
-	rec_dec	: process(clk_in, data_in, number) is
+	rec_dec	: process(clk_in) is
 	begin
 		if (clk_in'event and clk_in = '0') then
 		if (enable = '1') then
-			if (number < x"b") then
-				buff_data(number_i) <= data_in;
+			if (number < 12) then
+				buff_data(number) <= data_in;
+			else 
+				fin_c <= '1';
 			end if;
 		else
+			fin_c <= '0';
 			angle_barre_s <= buff_data;
 		end if;
 		end if;
 	end process rec_dec;
+
+	COMPT_FRONT : process(clk_in) is
+	begin
+		if (clk_in'event and clk_in = '1') then
+			if (enable = '1') then 
+				number <= number + 1;
+			end if;
+			if (number = 12) then
+				number <= 0;
+			end if;
+		end if;
+	end process COMPT_FRONT;
+
 	angle_barre <= angle_barre_s;
 end rtl;
