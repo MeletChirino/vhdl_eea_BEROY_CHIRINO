2cf3402c2eb35a8af82b327a5bcad3390119aec2
Melet David <melet.chirino@gmail.com>
2021-11-15 11:19:47 +0100

    [UPDATE] package file

--- BE_Barre_Franche/gestion_verin/package.vhdl
+++ BE_Barre_Franche/gestion_verin/package.vhdl
@@ -33,11 +33,11 @@ package tools is
 	end component;
 	component shift_register is
 		port(
-			enable		: in std_logic;
-			number		: in std_logic_vector(3 downto 0);
-			data_in		: in std_logic;
-			angle_barre	: out std_logic_vector(11 downto 0); 
-			clk_in		: in std_logic := '0'
+		enable		: in std_logic;
+		data_in		: in std_logic;
+		angle_barre	: out std_logic_vector(0 to 11);
+		fin_c		: out std_logic;
+		clk_in		: in std_logic
 		);
 	end component;
 	component compt_front is
@@ -49,10 +49,10 @@ package tools is
 	end component;
 	component pilote_adc is
 		port(
-			clk_in	: in std_logic;
-			cs_n	: in std_logic;
-			counter	: in std_logic_vector(3 downto 0);
-			enable	: out std_logic
+		clk_in	: in std_logic;
+		cs_n	: out std_logic;
+		fin_c	: in std_logic;
+		enable	: out std_logic
 		    );
 	end component;
 	component gene_start_conv is
