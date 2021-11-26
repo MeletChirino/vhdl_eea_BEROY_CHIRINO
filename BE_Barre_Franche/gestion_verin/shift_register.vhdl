abe607c6ac0fcda6975d6b04e1d13dc26700278f
EtienneLemaire <etienne.lemaire38@gmail.com>
2021-11-26 09:36:18 +0100

    [UPDATE] shift register working #13

--- BE_Barre_Franche/gestion_verin/shift_register.vhdl
+++ BE_Barre_Franche/gestion_verin/shift_register.vhdl
@@ -8,7 +8,7 @@ entity shift_register is
 	port(
 		cs_n		: in std_logic;
 		data_in		: in std_logic;
-		angle_barre	: out std_logic_vector(0 to 11);
+		angle_barre	: out std_logic_vector(11 downto 0);
 		fin_c		: out std_logic;
 		clk_in		: in std_logic
 	);
@@ -17,20 +17,20 @@ end entity;
 architecture rtl of shift_register is
 	signal buff_data	: std_logic_vector(0 to 11);
 	signal angle_barre_s	: std_logic_vector(0 to 11);
-	signal number		: integer range 0 to 16 := 0;
+	signal number		: integer range 0 to 17 := 0;
 begin
 	--rec_dec process
 	rec_dec	: process(clk_in) is
 	begin
 		if (clk_in'event and clk_in = '1') then
-			if (number < 3) then
+			if (number < 4) then
 				fin_c <= '0';
-			elsif (number > 2 and number < 15) then
-				buff_data(number - 3) <= data_in;
+			elsif (number > 3 and number < 16) then
+				buff_data(number - 4) <= data_in;
 				fin_c <= '0';
-			elsif (number = 15) then 
-				fin_c <= '1';
 			elsif (number = 16) then 
+				fin_c <= '1';
+			elsif (number = 17) then 
 				angle_barre_s <= buff_data;
 				fin_c <= '0';
 				end if;
@@ -40,7 +40,7 @@ begin
 	COMPT_FRONT : process(clk_in, cs_n) is
 	begin
 		if (clk_in'event and clk_in = '0') then
-			if (number < 16) then
+			if (number < 17) then
 				number <= number + 1;
 				end if;
 			if (cs_n = '1') then
@@ -50,5 +50,6 @@ begin
 			end if;
 	end process COMPT_FRONT;
 
-	angle_barre <= angle_barre_s;
+	angle_barre(11 downto 0) <= angle_barre_s(0 to 11);
+	
 end rtl;
